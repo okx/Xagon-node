77c2cd5e13c964701c45747e8f255090599dea46
JianGuo <zjg555543@163.com>
2024-03-31 11:20:06 +0800

    update

--- jsonrpc/types/types_xlayer.go
+++ jsonrpc/types/types_xlayer.go
@@ -1,22 +1,5 @@
 package types
 
-// BatchFilter is a list of batch numbers to retrieve
-type BatchFilter struct {
-	Numbers []BatchNumber `json:"numbers"`
-}
-
-// BatchData is an abbreviated structure that only contains the number and L2 batch data
-type BatchData struct {
-	Number      ArgUint64 `json:"number"`
-	BatchL2Data ArgBytes  `json:"batchL2Data,omitempty"`
-	Empty       bool      `json:"empty"`
-}
-
-// BatchDataResult is a list of BatchData for a BatchFilter
-type BatchDataResult struct {
-	Data []*BatchData `json:"data"`
-}
-
 // Contains checks if a string is contained in a slice of strings
 func Contains(s []string, str string) bool {
 	for _, v := range s {
