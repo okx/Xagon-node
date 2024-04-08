d36393ee14c18f74477a054d344b7ec8c75edcbe
hengfang.wang <hengfang.wang@okg.com>
2024-04-09 01:53:29 +0800

    feat(xlayer): reprocess

--- tools/state/reprocess_action.go
+++ tools/state/reprocess_action.go
@@ -180,7 +180,7 @@ func (r *reprocessAction) step(i uint64, oldStateRoot common.Hash, oldAccInputHa
 			block.IndexL1InfoTree: {
 				GlobalExitRoot: index.GlobalExitRoot.GlobalExitRoot,
 				BlockHashL1:    index.PreviousBlockHash,
-				MinTimestamp:   uint64(index.L1InfoTreeLeaf.Timestamp.Unix()),
+				MinTimestamp:   uint64(index.L1InfoTreeLeaf.GlobalExitRoot.Timestamp.Unix()),
 			},
 		}
 		log.Infof("deltatimestamp: %d, minTimestamp:%d, current: %d", block.DeltaTimestamp, index.GlobalExitRoot.Timestamp.Unix(), time.Now().Unix())
