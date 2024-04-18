ce9e3494e2d466907779218f5e2683f011e5d5bb
JianGuo <zjg555543@163.com>
2024-04-18 15:40:37 +0800

    update

--- sequencer/metrics_xlayer.go
+++ sequencer/metrics_xlayer.go
@@ -20,12 +20,12 @@ func getReasonFromBatch(batchCloseReason state.ClosingReason) BlockClosingReason
 
 // Summary returns the metrics summary.
 func (m *metrics) Summary(blockNum, batchNum, timestamp uint64) string {
-	TotalSequencerTime := "sequencer<" + fmt.Sprintf("%v", m.sequencerTime().Microseconds()) +
+	TotalSequencerTime := "SequencerTime<" + fmt.Sprintf("%v", m.sequencerTime().Microseconds()) +
 		"ms, newL2Block<" + fmt.Sprintf("%v", m.newL2BlockTimes.sequencer.Microseconds()) +
 		"ms>, txs<" + fmt.Sprintf("%v", m.transactionsTimes.sequencer.Microseconds()) +
 		"ms>, l2Block<" + fmt.Sprintf("%v", m.l2BlockTimes.sequencer.Microseconds()) + ">>, "
 
-	TotalExecutorTime := "executor<" + fmt.Sprintf("%v", m.executorTime().Microseconds()) +
+	TotalExecutorTime := "ExecutorTime<" + fmt.Sprintf("%v", m.executorTime().Microseconds()) +
 		"ms, newL2Block<" + fmt.Sprintf("%v", m.newL2BlockTimes.executor.Microseconds()) +
 		"ms>, txs<" + fmt.Sprintf("%v", m.transactionsTimes.executor.Microseconds()) +
 		"ms>, l2Block<" + fmt.Sprintf("%v", m.l2BlockTimes.executor.Microseconds()) + ">>, "
@@ -35,11 +35,11 @@ func (m *metrics) Summary(blockNum, batchNum, timestamp uint64) string {
 		"Timestamp<" + fmt.Sprintf("%v", timestamp) + ">, " +
 		"TxCount<" + fmt.Sprintf("%v", m.l2BlockTxsCount) + ">, " +
 		"Gas<" + fmt.Sprintf("%v", m.gas) + ">, " +
-		"time<" + fmt.Sprintf("%v", m.totalTime().Microseconds()) + "ms>, " +
-		"idleTime<" + fmt.Sprintf("%v", m.idleTime.Microseconds()) + "ms>, " +
+		"TotalTime<" + fmt.Sprintf("%v", m.totalTime().Microseconds()) + "ms>, " +
+		"IdleTime<" + fmt.Sprintf("%v", m.idleTime.Microseconds()) + "ms>, " +
 		TotalSequencerTime +
 		TotalExecutorTime +
-		"closeReason<" + m.closeReason + ">, "
+		"CloseReason<" + m.closeReason + ">, "
 
 	return result
 }
