8e813a704a07e7feeaac627f89ebfe18d7291ef4
ylsGit <bzwdyls@163.com>
2024-04-26 14:55:25 +0800

    debug info

--- state/trace.go
+++ state/trace.go
@@ -154,6 +154,7 @@ func (s *State) DebugTransaction(ctx context.Context, transactionHash common.Has
 		// Send Batch to the Executor
 		startTime = time.Now()
 		processBatchResponse, err := s.executorClient.ProcessBatch(ctx, processBatchRequest)
+		fmt.Printf("------ProcessBatch response:%+v\n", processBatchResponse)
 		endTime = time.Now()
 		if err != nil {
 			return nil, err
@@ -175,6 +176,7 @@ func (s *State) DebugTransaction(ctx context.Context, transactionHash common.Has
 		}
 
 		convertedResponse, err := s.convertToProcessBatchResponse(processBatchResponse)
+		fmt.Printf("------convertToProcessBatchResponse response:%+v\n", convertedResponse)
 		if err != nil {
 			return nil, err
 		}
@@ -310,6 +312,7 @@ func (s *State) DebugTransaction(ctx context.Context, transactionHash common.Has
 		// Send Batch to the Executor
 		startTime = time.Now()
 		processBatchResponseV2, err := s.executorClient.ProcessBatchV2(ctx, processBatchRequestV2)
+		fmt.Printf("------ProcessBatch V2 response:%+v\n", processBatchResponseV2)
 		endTime = time.Now()
 		if err != nil {
 			return nil, err
@@ -332,6 +335,7 @@ func (s *State) DebugTransaction(ctx context.Context, transactionHash common.Has
 		}
 
 		convertedResponse, err := s.convertToProcessBatchResponseV2(processBatchResponseV2)
+		fmt.Printf("------convertToProcessBatchResponse V2 response:%+v\n", convertedResponse)
 		if err != nil {
 			return nil, err
 		}
