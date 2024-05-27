d46f12cdcd2ce39ba722b1e78711ac452748ef11
zhangkai <zhangkai.gis@163.com>
2024-05-27 17:35:21 +0800

    add toString

--- jsonrpc/endpoints_eth_relay_xlayer.go
+++ jsonrpc/endpoints_eth_relay_xlayer.go
@@ -10,10 +10,20 @@ func (e *EthEndpoints) relayCall(method string, arg *types.TxArgs, blockArg *typ
 	dstURI := getRelayDestURI(e.cfg.ApiRelay.DestURI)
 	log.Infof("Relaying tx to %s %s %v %v", dstURI, method, arg, blockArg)
 
-	res, err := client.JSONRPCCall(dstURI, method, arg, blockArg)
+	res, err := client.JSONRPCCall(dstURI, method, arg, toString(blockArg))
 	if err != nil {
 		return RPCErrorResponse(types.DefaultErrorCode, "failed to relay tx to "+dstURI, err, true)
 	}
 
 	return res, nil
+}
+
+func toString(blockArg *types.BlockNumberOrHash) string {
+	if blockArg == nil {
+		return ""
+	}
+	if blockArg.IsHash() {
+		return blockArg.Hash().Hash().String()
+	}
+	return blockArg.Number().StringOrHex()
 }
