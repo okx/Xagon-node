2a531e7e8a965bbd3c3eeccac89a863c2a450880
chengzhinei <chengxue5yuan@163.com>
2024-05-30 17:04:57 +0800

    test

--- jsonrpc/endpoints_eth.go
+++ jsonrpc/endpoints_eth.go
@@ -1050,8 +1050,8 @@ func (e *EthEndpoints) tryToAddTxToPool(input, ip string) (interface{}, types.Er
 	}
 	log.Infof("adding TX to the pool: %v", tx.Hash().Hex())
 
-	dgp := getDynamicGp(e)
-	e.pool.AddDynamicGp(dgp)
+	//dgp := getDynamicGp(e)
+	//e.pool.AddDynamicGp(dgp)
 	if err := e.pool.AddTx(context.Background(), *tx, ip); err != nil {
 		// it's not needed to log the error here, because we check and log if needed
 		// for each specific case during the "pool.AddTx" internal steps
