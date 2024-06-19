4c1d262690551d69c1395b1efe28fd9b357eea2c
zhangkai <zhangkai.gis@163.com>
2024-06-19 18:26:56 +0800

    add the mock state xlayer

--- jsonrpc/mocks/mock_state_xlayer.go
+++ jsonrpc/mocks/mock_state_xlayer.go
@@ -86,3 +86,26 @@ func (_m *StateMock) GetBatchL2DataByNumbers(ctx context.Context, batchNumbers [
 
 	return r0, r1
 }
+
+func (_m *StateMock)GetLastL2BlockTimeByBatchNumber(ctx context.Context, batchNumber uint64, dbTx pgx.Tx) (uint64, error) {
+	ret := _m.Called(ctx, batchNumber, dbTx)
+
+	var r0 uint64
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, uint64, pgx.Tx) (uint64, error)); ok {
+		return rf(ctx, batchNumber, dbTx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, uint64, pgx.Tx) uint64); ok {
+		r0 = rf(ctx, batchNumber, dbTx)
+	} else {
+		r0 = ret.Get(0).(uint64)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, uint64, pgx.Tx) error); ok {
+		r1 = rf(ctx, batchNumber, dbTx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
