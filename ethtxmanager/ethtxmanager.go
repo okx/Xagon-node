b7341b418bc70132a646680009a30973c3bb0ebb
zhangkai <zhangkai.gis@163.com>
2024-10-13 15:43:56 +0800

    format imports

--- ethtxmanager/ethtxmanager.go
+++ ethtxmanager/ethtxmanager.go
@@ -13,13 +13,12 @@ import (
 	"sync"
 	"time"
 
+	"github.com/0xPolygonHermez/zkevm-node/ethtxmanager/metrics"
+	"github.com/0xPolygonHermez/zkevm-node/log"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/jackc/pgx/v4"
-
-	"github.com/0xPolygonHermez/zkevm-node/ethtxmanager/metrics"
-	"github.com/0xPolygonHermez/zkevm-node/log"
 )
 
 const (
